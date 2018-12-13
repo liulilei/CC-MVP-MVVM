1238d970fe458c7f0cf83154b901558434bcfc8e
liulilei <542910866@qq.com>
2018-12-13 11:40:42 +0800

    加载我的url

---
+++ desk_base_component/src/main/java/androiddesk/com/desk.base.component/base/WebViewFragment.kt
@@ -0,0 +1,105 @@
+package androiddesk.com.desk.base.component.base
+
+import android.graphics.Bitmap
+import android.os.Build
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androiddesk.com.desk.base.component.R
+import kotlinx.android.synthetic.main.fragment_web_view.*
+import me.yokeyword.fragmentation.SupportFragment
+
+/**
+ *@Description:
+ * @author: lll
+ * @date: 2018/12/13
+ */
+class WebViewFragment : SupportFragment() {
+
+    private var mRootView: ViewGroup? = null
+
+    companion object {
+
+        private const val ADDRESS = "address"
+
+        fun newInstance(address: String): WebViewFragment {
+            val args = Bundle()
+            val fragment = WebViewFragment()
+            args.putString(ADDRESS, address)
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        mRootView = View.inflate(context, R.layout.fragment_web_view, null) as ViewGroup
+        return mRootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViewAndData()
+    }
+
+    fun loadUrl(url: String) {
+        if (TextUtils.isEmpty(url) || webView == null) return
+        webView.loadUrl(url)
+    }
+
+    protected fun initViewAndData() {
+        val settings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.useWideViewPort = true
+        settings.layoutAlgorithm = WebSettings.LayoutAlgorithm.NORMAL
+        settings.builtInZoomControls = true
+        settings.loadWithOverviewMode = true
+        settings.domStorageEnabled = true
+        settings.pluginState = WebSettings.PluginState.ON
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+        }
+        webView.webChromeClient = WebChromeClient()
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                // 返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+                view.loadUrl(url)
+                return true
+            }
+
+            override fun onPageStarted(view: WebView, url: String, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                progressWheel.visibility = View.VISIBLE
+            }
+
+            override fun onPageFinished(view: WebView, url: String) {
+                super.onPageFinished(view, url)
+                progressWheel.visibility = View.GONE
+            }
+        }
+
+        val address = arguments!!.getString(ADDRESS)
+        if (!TextUtils.isEmpty(address)) {
+            webView.loadUrl(address)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mRootView?.removeView(webView)
+        webView.destroy()
+    }
+
+    fun webViewGoBack(): Boolean {
+        if (webView.canGoBack()) {
+            webView.goBack()// 返回前一个页面
+            return true
+        }
+        return false
+    }
+}
--- desk_my_component/src/main/java/com/androiddesk/desk/my/component/v/MyFragment.kt
+++ desk_my_component/src/main/java/com/androiddesk/desk/my/component/v/MyFragment.kt
@@ -2,7 +2,9 @@ package com.androiddesk.desk.newest.component.v
 
 import android.os.Bundle
 import androiddesk.com.desk.base.component.base.SimpleFragment
+import androiddesk.com.desk.base.component.base.WebViewFragment
 import com.androiddesk.desk.my.component.R
+import kotlinx.android.synthetic.main.base_title.*
 
 /**
  *@Description:
@@ -10,6 +12,10 @@ import com.androiddesk.desk.my.component.R
  * @date: 2018/12/10
  */
 class MyFragment : SimpleFragment() {
+
+    private var mWebViewFragment: WebViewFragment? = null
+
+    private val url = "https://github.com/liulilei/CC-MVP-MVVM"
 
     companion object {
         fun newInstance(): MyFragment {
@@ -22,5 +28,20 @@ class MyFragment : SimpleFragment() {
 
     override fun initLayout(): Int {
         return R.layout.fragment_my
+    }
+
+    override fun initViewAndData() {
+        titleTv.text = "我的"
+        loadRootFragment(R.id.fl_container,
+                mWebViewFragment.let {
+                    mWebViewFragment = WebViewFragment.newInstance(url)
+                    return@let mWebViewFragment
+                })
+    }
+
+    override fun onBackPressedSupport(): Boolean {
+        return if (mWebViewFragment != null && mWebViewFragment?.webViewGoBack()!!) {
+            true
+        } else super.onBackPressedSupport()
     }
 }
