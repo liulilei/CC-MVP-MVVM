b90a6b1ec1918da5e813376cf92cf31b0c8a40e6
liulilei <542910866@qq.com>
2018-12-16 18:30:41 +0800

    详情act

---
+++ desk_detail_component/src/main/java/com/androiddesk/desk/detail/component/DeskDetailActivity.kt
@@ -0,0 +1,14 @@
+package com.androiddesk.desk.detail.component
+
+import androiddesk.com.desk.base.component.base.SimpleActivity
+
+/**
+ *@Description:
+ * @author: lll
+ * @date: 2018/12/16
+ */
+class DeskDetailActivity : SimpleActivity() {
+    override fun initLayout(): Int {
+        return R.layout.desk_detail
+    }
+}
